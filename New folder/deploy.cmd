f621ee292bdbf400d0c7d8b444f7f2698cfcdd28
Md. Azharul Hoque <azharul.hoque@televersant.com>
2018-07-30 20:38:40 +0600

    configure pg

--- New folder/deploy.cmd
+++ New folder/deploy.cmd
@@ -49,35 +49,6 @@ IF NOT DEFINED KUDU_SYNC_CMD (
 )
 goto Deployment
 
-:: Utility Functions
-:: -----------------
-
-:SelectPythonVersion
-
-IF DEFINED KUDU_SELECT_PYTHON_VERSION_CMD (
-  call %KUDU_SELECT_PYTHON_VERSION_CMD% "%DEPLOYMENT_SOURCE%" "%DEPLOYMENT_TARGET%" "%DEPLOYMENT_TEMP%"
-  IF !ERRORLEVEL! NEQ 0 goto error
-
-  SET /P PYTHON_RUNTIME=<"%DEPLOYMENT_TEMP%\__PYTHON_RUNTIME.tmp"
-  IF !ERRORLEVEL! NEQ 0 goto error
-
-  SET /P PYTHON_VER=<"%DEPLOYMENT_TEMP%\__PYTHON_VER.tmp"
-  IF !ERRORLEVEL! NEQ 0 goto error
-
-  SET /P PYTHON_EXE=<"%DEPLOYMENT_TEMP%\__PYTHON_EXE.tmp"
-  IF !ERRORLEVEL! NEQ 0 goto error
-
-  SET /P PYTHON_ENV_MODULE=<"%DEPLOYMENT_TEMP%\__PYTHON_ENV_MODULE.tmp"
-  IF !ERRORLEVEL! NEQ 0 goto error
-) ELSE (
-  SET PYTHON_RUNTIME=python-2.7
-  SET PYTHON_VER=2.7
-  SET PYTHON_EXE=%SYSTEMDRIVE%\python27\python.exe
-  SET PYTHON_ENV_MODULE=virtualenv
-)
-
-goto :EOF
-
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Deployment
 :: ----------
@@ -98,7 +69,8 @@ echo Detected requirements.txt. RUNNING CUSTOM DEPLOYMENT
 
 :: 2. Install packages
 echo Pip install requirements.
-D:\home\Python27\python.exe -m pip install --upgrade -r requirements.txt
+D:\home\python362x86\python.exe -m pip install -I setuptools
+D:\home\python362x86\python.exe -m pip install --upgrade -r requirements.txt
 IF !ERRORLEVEL! NEQ 0 goto error
 
 
